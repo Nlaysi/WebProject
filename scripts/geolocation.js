0ab4fabb77da8473cb979f94f11e801067697255
Nlaysi <62209572+Nlaysi@users.noreply.github.com>
2021-04-02 09:07:30 +0300

    Update geolocation.js
    https

--- scripts/geolocation.js
+++ scripts/geolocation.js
@@ -3,17 +3,17 @@ var value;
 if(localStorage.getItem("city") == null) {
     navigator.geolocation.getCurrentPosition(
         function(position) {
-            var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&units=metric&appid=a013324cdcd65a54ee5b6c5c40271e96";
+            var url = "https://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&units=metric&appid=a013324cdcd65a54ee5b6c5c40271e96";
             getMainWeather(url);
         }
         ,
         function error() {
-            var url = "http://api.openweathermap.org/data/2.5/weather?q=Saint%20Petersburg&units=metric&appid=a013324cdcd65a54ee5b6c5c40271e96";
+            var url = "https://api.openweathermap.org/data/2.5/weather?q=Saint%20Petersburg&units=metric&appid=a013324cdcd65a54ee5b6c5c40271e96";
             getMainWeather(url);
         }
     )
 }
 else {
-    var url = "http://api.openweathermap.org/data/2.5/weather?q=" + localStorage.getItem("city") + "&units=metric&appid=a013324cdcd65a54ee5b6c5c40271e96";
+    var url = "https://api.openweathermap.org/data/2.5/weather?q=" + localStorage.getItem("city") + "&units=metric&appid=a013324cdcd65a54ee5b6c5c40271e96";
     getMainWeather(url);
 }
